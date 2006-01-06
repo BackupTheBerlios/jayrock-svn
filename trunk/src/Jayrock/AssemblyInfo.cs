9b450d6e334ede7f5e403e11c772df677f306ed8
atifaziz <atifaziz@9d242436-c8ff-0310-b461-824ead7a104a>
2006-01-06 11:19:56 +0000

    * Changed assembly and file version based on recent changes. * The assembly version has changed because of a breaking change in how the json.js script is expected by the test page.

--- trunk/src/Jayrock/AssemblyInfo.cs
+++ trunk/src/Jayrock/AssemblyInfo.cs
@@ -43,8 +43,8 @@ using ComVisible = System.Runtime.InteropServices.ComVisibleAttribute;
 // Version information
 //
 
-[assembly: AssemblyVersion("0.9.6826.0")]
-[assembly: AssemblyFileVersion("0.9.7119.0")]
+[assembly: AssemblyVersion("0.9.7206.0")]
+[assembly: AssemblyFileVersion("0.9.7206.0")]
 
 //
 // Configuration (test, debug, release)
