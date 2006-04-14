2600af7453d65c357dbc9d4e5b53f382e7225a21
atifaziz <atifaziz@9d242436-c8ff-0310-b461-824ead7a104a>
2006-04-14 10:02:03 +0000

    * Update AssemblyInfo.cs version to reflect the change in previous commit.

--- trunk/src/Jayrock/AssemblyInfo.cs
+++ trunk/src/Jayrock/AssemblyInfo.cs
@@ -43,8 +43,8 @@ using ComVisible = System.Runtime.InteropServices.ComVisibleAttribute;
 // Version information
 //
 
-[assembly: AssemblyVersion("0.9.7507.0")]
-[assembly: AssemblyFileVersion("0.9.7507.0")]
+[assembly: AssemblyVersion("0.9.7514.0")]
+[assembly: AssemblyFileVersion("0.9.7514.0")]
 
 //
 // Configuration (test, debug, release)
