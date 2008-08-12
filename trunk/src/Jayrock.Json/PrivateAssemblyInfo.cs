48e483363e21ef925eae58c54fbdac9fad5dfd16
atifaziz <atifaziz@9d242436-c8ff-0310-b461-824ead7a104a>
2008-08-12 21:56:58 +0000

    * Simplified implementation of UnixTime.

--- trunk/src/Jayrock.Json/PrivateAssemblyInfo.cs
+++ trunk/src/Jayrock.Json/PrivateAssemblyInfo.cs
@@ -34,4 +34,4 @@ using ComVisible = System.Runtime.InteropServices.ComVisibleAttribute;
 // Version information
 //
 
-[assembly: AssemblyFileVersion("0.9.10312.1548")]
+[assembly: AssemblyFileVersion("0.9.10312.2155")]
